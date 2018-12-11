8c025d8b2c18eafe803b92fe6fb47b646c858bf3
sourabh <sourabh@inubesolutions.com>
2018-12-11 21:21:29 +0530

    Adding reactive forms and validations to app.componet.html and app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularHealth';
+  loginForm : FormGroup;
+  
+  constructor(private _formBuilder : FormBuilder){}
+
+  ngOnInit() : void {
+    this.loginForm = this._formBuilder.group({
+      userName : ['',Validators.required],
+      passwords: ['', Validators.required]
+    })
+  }
 }
