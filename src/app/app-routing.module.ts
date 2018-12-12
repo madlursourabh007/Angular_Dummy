51dc8d9e446e2d125520a426b6365ae7274ad21e
sourabh <sourabh@inubesolutions.com>
2018-12-12 16:22:32 +0530

    Adding lazy loading and making signup module as future module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from 'src/app/enduser/pages/signup/signup.component';
 import { LoginComponent } from 'src/app/login/login/login.component';
+import { SignupModule } from '../app/enduser/pages/signup/signup.module';
 
 const routes: Routes = [
   {
     path : 'signup',
-    component : SignupComponent
+    loadChildren : '../app/enduser/pages/signup/signup.module#SignupModule'
   },
   {
     path : 'login',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { SignupComponent } from './enduser/pages/signup/signup.component';
 import { LoginComponent } from './login/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SignupComponent,
     LoginComponent
   ],
   imports: [
